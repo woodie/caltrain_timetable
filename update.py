3708aa8867507b7689388bf217ffa6e83f147e29
John Woodell <woodie@netpress.com>
2019-01-26 14:15:55 -0800

    Cleanup shedule parser

--- update.py
+++ update.py
@@ -12,10 +12,10 @@ def main():
   fetch_schedule_data()
   stations = parse_station_data()
   trips = parse_schedule_data(stations)
-  write_schedule_file('North M-F',  'nb', trips['weekday'], stations)
-  write_schedule_file('South M-F',  'sb', trips['weekday'], stations)
-  write_schedule_file('North S-Su', 'nb', trips['weekend'], stations)
-  write_schedule_file('South S-Su', 'sb', trips['weekend'], stations)
+  write_schedule_file('nb', 'weekday', trips, stations)
+  write_schedule_file('sb', 'weekday', trips, stations)
+  write_schedule_file('nb', 'weekend', trips, stations)
+  write_schedule_file('sb', 'weekend', trips, stations)
 
 def fetch_schedule_data():
   source = 'http://www.caltrain.com/Assets/GTFS/caltrain/CT-GTFS.zip'
@@ -31,25 +31,24 @@ def fetch_schedule_data():
   os.chdir(basedir)
 
 def parse_station_data():
-  stations = {'nb':[], 'sb':[], 'labels':{}}
-  extraneous = ['Diridon', 'Caltrain', 'Station']
+  _stations = {'nb':[], 'sb':[], 'labels':{}}
   with open('CT-GTFS/stops.txt', 'rb') as stopsFile:
     stopsReader = csv.reader(stopsFile)
     stopHeaders = next(stopsReader, None)
     for row in stopsReader:
       stop_id = int(row[1])
-      for words in extraneous:
+      for words in ['Diridon', 'Caltrain', 'Station']:
         row[2] = row[2].replace(words, '')
-      stations['labels'][stop_id] = re.sub('\s+', ' ', row[2]).strip()
+      _stations['labels'][stop_id] = re.sub('\s+', ' ', row[2]).strip()
       if (stop_id % 2 == 1):
-        stations['nb'].insert(0, stop_id)
+        _stations['nb'].insert(0, stop_id)
       else:
-        stations['sb'].append(stop_id)
-  return stations
+        _stations['sb'].append(stop_id)
+  return _stations
 
 def parse_schedule_data(stations):
-  weekday = {'nb':OrderedDict(), 'sb':OrderedDict()}
-  weekend = {'nb':OrderedDict(), 'sb':OrderedDict()}
+  _trips = {'weekday':{'nb':OrderedDict(), 'sb':OrderedDict()},
+            'weekend':{'nb':OrderedDict(), 'sb':OrderedDict()}}
   with open('CT-GTFS/stop_times.txt', 'rb') as timesFile:
     timesReader = csv.reader(timesFile)
     timeHeaders = next(timesReader, None)
@@ -63,35 +62,23 @@ def parse_schedule_data(stations):
       ampm = 'PM' if (hour > 11 and hour < 24) else 'AM'
       hr = hour - 12 if (hour > 12) else hour
       departure = "%s:%s %s" % (hr, minute, ampm)
-      if (stop_id % 2 == 1):
-        if (trip_id < 400):
-          if (trip_id not in weekday['nb']):
-            weekday['nb'][trip_id] = [None] * len(stations['nb'])
-          weekday['nb'][trip_id][stations['nb'].index(stop_id)] = departure
-        else:
-          if (trip_id not in weekend['nb']):
-            weekend['nb'][trip_id] = [None] * len(stations['nb'])
-          weekend['nb'][trip_id][stations['nb'].index(stop_id)] = departure
-      else:
-        if (trip_id < 400):
-          if (trip_id not in weekday['sb']):
-            weekday['sb'][trip_id] = [None] * len(stations['sb'])
-          weekday['sb'][trip_id][stations['sb'].index(stop_id)] = departure
-        else:
-          if (trip_id not in weekend['sb']):
-            weekend['sb'][trip_id] = [None] * len(stations['sb'])
-          weekend['sb'][trip_id][stations['sb'].index(stop_id)] = departure
-  return {'weekday':weekday, 'weekend':weekend}
+      direction = 'nb' if (stop_id % 2 == 1) else 'sb'
+      schedule = 'weekday' if (trip_id < 400) else 'weekend'
+      if (trip_id not in _trips[schedule][direction]):
+        _trips[schedule][direction][trip_id] = [None] * len(stations[direction])
+      _trips[schedule][direction][trip_id][stations[direction].index(stop_id)] = departure
+  return _trips
 
-def write_schedule_file(segment, direction, trips, stations):
-  with open('res/CalTrain@%s.txt' % segment, 'w') as f:
+def write_schedule_file(direction, schedule, trips, stations):
+  ex = {'nb':'North', 'sb':'South', 'weekday':'M-F', 'weekend':'S-Su'}
+  with open('res/CalTrain@%s %s.txt' % (ex[direction], ex[schedule]), 'w') as f:
     header = ['Train No.']
     for stop_id in stations[direction]:
       header.append(stations['labels'][stop_id])
     f.write('\t'.join(header))
     f.write('\n')
-    for trip_id in trips[direction]:
-      f.write('\t'.join(map(xstr,[str(trip_id)] + trips[direction][trip_id])))
+    for trip_id in trips[schedule][direction]:
+      f.write('\t'.join(map(xstr,[str(trip_id)] + trips[schedule][direction][trip_id])))
       f.write('\n')
 
 
