6c803cd72d15a2b9a53a90b5f8357397ff6d2394
John Woodell <woodie@netpress.com>
2019-01-26 22:28:41 -0800

    Use better hash key names.

--- update.py
+++ update.py
@@ -12,10 +12,10 @@ def main():
   fetch_schedule_data()
   stations = parse_station_data()
   trips = parse_schedule_data(stations)
-  write_schedule_file('nb', 'weekday', trips, stations)
-  write_schedule_file('sb', 'weekday', trips, stations)
-  write_schedule_file('nb', 'weekend', trips, stations)
-  write_schedule_file('sb', 'weekend', trips, stations)
+  write_schedule_file('north', 'weekday', trips, stations)
+  write_schedule_file('south', 'weekday', trips, stations)
+  write_schedule_file('north', 'weekend', trips, stations)
+  write_schedule_file('south', 'weekend', trips, stations)
 
 def fetch_schedule_data():
   source = 'http://www.caltrain.com/Assets/GTFS/caltrain/CT-GTFS.zip'
@@ -31,7 +31,7 @@ def fetch_schedule_data():
   os.chdir(basedir)
 
 def parse_station_data():
-  _stations = {'nb':[], 'sb':[], 'labels':{}}
+  _stations = {'north':[], 'south':[], 'labels':{}}
   with open('CT-GTFS/stops.txt', 'rb') as stopsFile:
     stopsReader = csv.reader(stopsFile)
     stopHeaders = next(stopsReader, None)
@@ -41,14 +41,14 @@ def parse_station_data():
         row[2] = row[2].replace(words, '')
       _stations['labels'][stop_id] = re.sub('\s+', ' ', row[2]).strip()
       if (stop_id % 2 == 1):
-        _stations['nb'].insert(0, stop_id)
+        _stations['north'].insert(0, stop_id)
       else:
-        _stations['sb'].append(stop_id)
+        _stations['south'].append(stop_id)
   return _stations
 
 def parse_schedule_data(stations):
-  _trips = {'weekday':{'nb':OrderedDict(), 'sb':OrderedDict()},
-            'weekend':{'nb':OrderedDict(), 'sb':OrderedDict()}}
+  _trips = {'weekday':{'north':OrderedDict(), 'south':OrderedDict()},
+            'weekend':{'north':OrderedDict(), 'south':OrderedDict()}}
   with open('CT-GTFS/stop_times.txt', 'rb') as timesFile:
     timesReader = csv.reader(timesFile)
     timeHeaders = next(timesReader, None)
@@ -62,7 +62,7 @@ def parse_schedule_data(stations):
       ampm = 'PM' if (hour > 11 and hour < 24) else 'AM'
       hr = hour - 12 if (hour > 12) else hour
       departure = "%s:%s %s" % (hr, minute, ampm)
-      direction = 'nb' if (stop_id % 2 == 1) else 'sb'
+      direction = 'north' if (stop_id % 2 == 1) else 'south'
       schedule = 'weekday' if (trip_id < 400) else 'weekend'
       if (trip_id not in _trips[schedule][direction]):
         _trips[schedule][direction][trip_id] = [None] * len(stations[direction])
@@ -70,8 +70,8 @@ def parse_schedule_data(stations):
   return _trips
 
 def write_schedule_file(direction, schedule, trips, stations):
-  ex = {'nb':'North', 'sb':'South', 'weekday':'M-F', 'weekend':'S-Su'}
-  with open('res/CalTrain@%s %s.txt' % (ex[direction], ex[schedule]), 'w') as f:
+  days = 'M-F' if (schedule == 'north') else 'S-Su'
+  with open('res/CalTrain@%s %s.txt' % (direction.capitalize(), days), 'w') as f:
     header = ['Train No.']
     for stop_id in stations[direction]:
       header.append(stations['labels'][stop_id])
